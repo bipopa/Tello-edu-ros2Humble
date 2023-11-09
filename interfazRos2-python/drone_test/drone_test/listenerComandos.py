19d348783c0c92fcbf91f24ab40d0d7ea6d7a496
vagrant <vagrant@pythonMachine>
2023-11-09 02:54:36 +0000

    Se agrego el packete inicial de ROS2

---
+++ interfazRos2-python/drone_test/drone_test/listenerComandos.py
@@ -0,0 +1,32 @@
+import rclpy
+from std_msgs.msg import String
+import socket
+
+def enviar_comando(mensaje):
+    # Crea un socket UDP
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Dirección y puerto del servidor
+    server_address = ('192.168.10.1', 8889)
+
+    # Envía los datos al servidor
+    client_socket.sendto(mensaje.encode(), server_address)
+
+    # Cierra el socket
+    client_socket.close()
+
+def callback(msg):
+    # Llamada a la función para enviar comandos
+    enviar_comando("command")
+    time.sleep(0.1)
+    enviar_comando(msg.data)
+
+def main():
+    rclpy.init()
+    node = rclpy.create_node('tello_controller_node')
+    subscriber = node.create_subscription(String, 'comandos', callback, 10)
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ interfazRos2-python/drone_test/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+package_name = 'drone_test'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='vagrant',
+    maintainer_email='vagrant@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+                'talker = drone_test.comandos_test:main',
+                'listener = drone_test.listener_test:main',
+                'listener_comandos = drone_test.listenerComandos:main',
+        ],
+},
+)
